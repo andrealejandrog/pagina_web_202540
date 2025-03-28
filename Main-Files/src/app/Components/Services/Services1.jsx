ee5608ff6fd3d946acfa2cf2f8060be41b07dad2
Jefrey Toledo <jefreytoledo1998@gmail.com>
2025-03-28 11:44:15 -0600

    Feature: añadir seccion de impacto

--- Main-Files/src/app/Components/Services/Services1.jsx
+++ Main-Files/src/app/Components/Services/Services1.jsx
@@ -1,47 +1,46 @@
-// Usado para seccion de impactos
 import SectionTitle from "../Common/SectionTitle";
-import data from '../../Data/Home1/services1.json';
-import Link from "next/link";
+import data from '../../Data/Home1/impacto.json';
 import Image from "next/image";
 
 const Services1 = () => {
     return (
-        <div className="service sp" id = 'impacto'>
+        <div className="service sp" id='impacto'>
           <div className="container">
             <div className="row">
               <div className="col-lg-8 m-auto text-center">
                 <div className="heading1">
                     <SectionTitle
-                        SubTitle="Nuestros servicios"
-                        Title="Empower Your Business With Our Comprehensive Technology & IT Solutions"
-                    ></SectionTitle>
+                        SubTitle="5-0techonology"
+                        Title="Protege, Optimiza y Crece"
+                    />
                 </div>
               </div>
             </div>
 
             <div className="space30"></div>
             <div className="row">
-            {data.map((item, i) => (
-              <div key={i} className={item.addClass} data-aos="zoom-in-up" data-aos-duration="700">
-                <div className="single-box">
-                  <div className="icon">
-                  <Image src={item.icon} alt="img" width={39} height={40}   />
-                  </div>
-                  <div className="heading1">
-                    <h4><Link href={item.btnLink}>{item.title}</Link></h4>
-                    <div className="space16"></div>
-                    <p>{item.desc}</p>
-                    <div className="space16"></div>
-                    <Link href={item.btnLink} className="learn">{item.btnText} <span>
-                      <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-arrow-right-short" viewBox="0 0 16 16">
-                        <path fillRule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8"/>
-                      </svg>
-                    </span></Link>
+              {data.map((category, i) => (
+                <div key={i} className={category.addClass} data-aos="zoom-in-up" data-aos-duration="700">
+                  <div className="single-box">
+                    <div className="icon">
+                      <Image src={category.icon} alt={category.title} width={39} height={40} />
+                    </div>
+                    <div className="heading1">
+                      <h4>{category.title}</h4>
+                      <p className="category-desc">{category.desc}</p>
+                      
+                      <ul className="service-items-list">
+                        {category.items.map((item, itemIndex) => (
+                          <li key={itemIndex}>
+                            <span className="service-bullet">●</span>
+                            {item}
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
                   </div>
                 </div>
-              </div>
-                ))}
-              
+              ))}
             </div>
           </div>
         </div>
