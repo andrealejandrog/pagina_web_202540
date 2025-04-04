7803709475e0e8ade10967591dffb917119cb616
Jefrey Toledo <jefreytoledo1998@gmail.com>
2025-04-03 20:39:17 -0600

    Feature: cambiando tipografía

--- Main-Files/src/app/layout.js
+++ Main-Files/src/app/layout.js
@@ -1,36 +1,33 @@
-import { Figtree } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import "slick-carousel/slick/slick.css";
 import "./assets/main.css";
 
-const figtree = Figtree({
+// Configura Montserrat con peso 600 como única variante
+const montserrat = Montserrat({
   subsets: ['latin'],
-  weight: ['300','400', '500', '600', '700', '800', '900'],
-  variable: '--body-color-font',
+  weight: '600',
+  variable: '--body-font',
 });
 
 export const metadata = {
   title: {
     absolute: '',
     default: '50 solutions - Innovamos con propósito, transformamos con tecnología',
-    template: '%s | 50 solutions  - Innovamos con propósito, transformamos con tecnología',
+    template: '%s | 50 solutions - Innovamos con propósito, transformamos con tecnología',
   },
-  description: '50 solutions  - TInnovamos con propósito, transformamos con tecnología',
-  openGraph: {
-    title: '50 solutions  - Innovamos con propósito, transformamos con tecnología',
-    description: '50 solutions  - Innovamos con propósito, transformamos con tecnología',
-  },
+  description: '50 solutions - Innovamos con propósito, transformamos con tecnología',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="es"> {/* Cambiado a "es" para español */}
       <head>
         <meta name="author" content="Themeservices" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
-      <body className={`${figtree.variable}`}>
+      <body className={montserrat.className}>
         {children}
       </body>
     </html>
