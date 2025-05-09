5e08a341b1aac9fafc20a58816570411509f7ea7
Jefrey Toledo <jefreytoledo1998@gmail.com>
2025-05-09 13:11:03 -0600

    Fix: showing language button in mobile

--- Main-Files/src/app/Components/Header/Header1.jsx
+++ Main-Files/src/app/Components/Header/Header1.jsx
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import Nav from './Nav'
 import Link from 'next/link'
 import Image from 'next/image'
@@ -14,13 +14,8 @@ export default function Header1({ variant }) {
 
   return (
     <div className='header-area2 header_nav_03'>
-      <header
-        className={`cs_site_header cs_style_1 ${
-          variant ? variant : ''
-        } cs_sticky_header cs_site_header_full_width ${
-          mobileToggle ? 'cs_mobile_toggle_active' : ''
-        }`}
-      >
+      <header className={`cs_site_header cs_style_1 ${variant || ''} cs_sticky_header cs_site_header_full_width ${mobileToggle ? 'cs_mobile_toggle_active' : ''}`}>
+        {/* Top Header (Misión) */}
         <div className="cs_top_header">
           <div className="container">
             <div className="row">
@@ -32,30 +27,34 @@ export default function Header1({ variant }) {
             </div>
           </div>
         </div>
-        
+
+        {/* Main Header */}
         <div className="cs_main_header cs_accent_bg">
           <div className="container">
             <div className="cs_main_header_in">
+              {/* Logo + Idioma móvil */}
               <div className="cs_main_header_left">
                 <Link className="cs_site_branding" href="/">
-                  <Image
-                    src={Logo}
-                    alt="5-0 Technology - Soluciones Tecnológicas"
-                    width={115}
-                    height={23}
-                    priority
-                  />
+                  <Image src={Logo} alt="5-0 Technology" width={115} height={23} priority />
                 </Link>
+
+                {/* Botón de idioma móvil con mejor espaciado */}
+                <div className="d-lg-none mobile-language-toggle ms-3">  {/* Añadido ms-3 para margen */}
+                  <button
+                    onClick={toggleLanguage}
+                    className="cs_language_toggle cs_language_text"
+                    aria-label="Toggle language"
+                  >
+                    {language === 'es' ? 'ENG' : 'ES'}
+                  </button>
+                </div>
               </div>
 
+              {/* Navegación principal */}
               <div className="cs_main_header_center1">
                 <div className="cs_nav cs_primary_font fw-medium">
                   <span
-                    className={
-                      mobileToggle
-                        ? 'cs-munu_toggle cs_teggle_active'
-                        : 'cs-munu_toggle'
-                    }
+                    className={mobileToggle ? 'cs-munu_toggle cs_teggle_active' : 'cs-munu_toggle'}
                     onClick={() => setMobileToggle(!mobileToggle)}
                   >
                     <span></span>
@@ -64,24 +63,27 @@ export default function Header1({ variant }) {
                 </div>
               </div>
 
-              <div className="cs_main_header_right header_right_one">
+              {/* Elementos derecha (desktop) */}
+              <div className="cs_main_header_right header_right_one d-none d-lg-flex">
+                {/* Idioma desktop */}
                 <div className="language-selector me-3">
-                  <button 
+                  <button
                     onClick={toggleLanguage}
-                    className="cs_language_toggle"
+                    className="cs_language_toggle cs_language_text"
                   >
-                    {t('header.language_toggle')}
+                    {language === 'es' ? 'ENG' : 'ES'}
                   </button>
                 </div>
 
+                {/* Contacto */}
                 <div className="header1-buttons">
                   <div className="contact-btn">
                     <div className="icon">
-                      <Image 
-                        src="/assets/img/icons/header1-icon.png" 
-                        alt="img" 
-                        width={22} 
-                        height={22}   
+                      <Image
+                        src="/assets/img/icons/header1-icon.png"
+                        alt="Contact"
+                        width={22}
+                        height={22}
                       />
                     </div>
                     <div className="headding">
