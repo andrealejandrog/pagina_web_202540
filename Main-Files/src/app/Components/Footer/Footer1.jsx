2c71dc88f961b29474ae661644faacd9fa2b0145
Jefrey Toledo <jefreytoledo1998@gmail.com>
2025-05-20 15:57:06 -0600

    Feat: changing style for footer

--- Main-Files/src/app/Components/Footer/Footer1.jsx
+++ Main-Files/src/app/Components/Footer/Footer1.jsx
@@ -1,108 +1,48 @@
 'use client'
 import Image from "next/image";
-import { useTranslations } from '../../hooks/useTranslations'
-// import { useTranslations } from '../../hooks/useTranslations';
+import { useTranslations } from '../../hooks/useTranslations';
 
 const Footer1 = () => {
-  const { t } = useTranslations()
-    return (
-        <div className="footer1 _relative" id ='footer'>
-        <div className="container">
-             <div className="row">
-                  <div className="col-lg-6">
-                       <div className="single-footer-items footer-logo-area">
-                            <div className="footer-logo">
-                              <Image
-                              src="/assets/img/logo/Original_logo.svg"
-                              alt="img"
-                              width={350}
-                              height={10}
-                              />
-                            </div>
-                            <div className="heading1">
-                              <p>{t('footer.slogan')}</p>
-                            </div>
-                            <ul className="social-icon">
-                                 <li><a href="#"><i className="bi bi-linkedin"></i></a></li>
-                                 <li><a href="#"><i className="bi bi-twitter-x"></i></a></li>
-                                 <li><a href="#"><i className="bi bi-facebook"></i></a></li>
-                                 <li><a href="#"><i className="bi bi-instagram"></i></a></li>
-                            </ul>
-                       </div>
-                  </div>
-                  <div className="col-lg-6">
-                       <div className="single-footer-items">
-                            <h3>{t('footer.contact_title')}</h3>
-
-                            <div className="contact-box">
-                              <div className="icon">
-                                <i className="bi bi-geo-alt-fill"></i>
-                              </div>
-                              <div className="pera">
-                                   <p>Guatemala: Condado El Naranjo 23 Calle 14-58 zona 4 Mixco Guatemala.</p>
-                              </div>
-                            </div>
-                            <div className="contact-box">
-                              <div className="icon">
-                                <i className="bi bi-geo-alt-fill"></i>
-                              </div>
-                              <div className="pera">
-                                   <p>US: 306 East 17th St, Lumberton, NC 28358.</p>
-                              </div>
-                            </div>
-
-                            <div className="contact-box">
-                              <div className="icon">
-                              {/* <Image src="/assets/img/icons/footer1-icon2.png" alt="img" width={24} height={24}   /> */}
-                              <i className="bi bi-telephone-outbound-fill"></i>
-                              </div>
-                              <div className="pera">
-                                <a href="tel:+50237580290">+502 3758-0290</a>
-                              </div>
-                            </div>
-
-                            <div className="contact-box">
-                              <div className="icon">
-                              {/* <Image src="/assets/img/icons/footer1-icon3.png" alt="img" width={24} height={24}   /> */}
-                              <i className="bi bi-envelope-paper-fill"></i>
-                              </div>
-                              <div className="pera">
-                                <a href="mailto:info@50-technology.com">info@50-technology.com</a>
-                              </div>
-                            </div>
-
-                            <div className="contact-box">
-                              <div className="icon">
-                              {/* <Image src="/assets/img/icons/footer1-icon4.png" alt="img" width={24} height={24}/>                                 */}
-                              <i className="bi bi-laptop-fill"></i>
-                              </div>
-                              <div className="pera">
-                                <a href="mailto:50-technology.com">50-technology.com</a>
-                              </div>
-                            </div>
-
-                       </div>
-                  </div>
-
-             </div>
-
-             <div className="space40"></div>
-        </div>
-
-        <div className="copyright-area">
-          <div className="container">
-            <div className="row align-items-center">
-              <div className="col-md-4">
-                    <div className="coppyright">
-                         <p>Copyright @2025 5.0 Technology. All Rights Reserved</p>
-                    </div>
-               </div>
-               </div>
+  const { t } = useTranslations();
+  return (
+    <div className="footer1 _relative" id='footer'>
+      <div className="container">
+        <div className="row footer-main-content align-items-center"> {/* Fila principal con alineación vertical */}
+          
+          {/* Columna Izquierda: Logo, Slogan, Copyright */}
+          <div className="col-lg-6 footer-left-content">
+            <div className="footer-logo mb-3"> {/* mb-3 para espacio debajo del logo */}
+              <Image
+                src="/assets/img/logo/Original_logo.svg"
+                alt="5.0 Technology Logo"
+                width={350} // Ancho ajustado para el logo
+                height={10} // Altura ajustada (o la que corresponda a tu logo para mantener proporción)
+                            // Si tu SVG escala bien, podrías omitir height y controlarlo con CSS.
+              />
+            </div>
+            <div className="footer-slogan mb-3"> {/* mb-3 para espacio debajo del slogan */}
+              <p>{t('footer.slogan')}</p>
+            </div>
+            <div className="footer-copyright">
+              <p>Copyright @2025 5.0 Technology. All Rights Reserved</p>
+            </div>
           </div>
-        </div>
 
+          {/* Columna Derecha: Título "Follow us" e Iconos Sociales */}
+          <div className="col-lg-6 footer-right-content">
+            <h3 className="footer-follow-us-title">{t('footer.contact_title')}</h3>
+            <ul className="social-icon-list">
+              <li><a href="#" aria-label="LinkedIn"><i className="bi bi-linkedin"></i></a></li>
+              <li><a href="#" aria-label="Twitter X"><i className="bi bi-twitter-x"></i></a></li>
+              <li><a href="#" aria-label="Facebook"><i className="bi bi-facebook"></i></a></li>
+              <li><a href="#" aria-label="Instagram"><i className="bi bi-instagram"></i></a></li>
+            </ul>
+          </div>
+
+        </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Footer1;
